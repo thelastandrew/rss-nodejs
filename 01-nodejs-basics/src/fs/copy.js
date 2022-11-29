c40c5d5eeb949a0706e94e7d2ec931e9ec8fadb8
thelastandrew <thelastandrew@gmail.com>
2022-11-29 22:12:10 +0300

    feat: fs-copy resolved

--- 01-nodejs-basics/src/fs/copy.js
+++ 01-nodejs-basics/src/fs/copy.js
@@ -1,5 +1,20 @@
+import { readdir, mkdir, copyFile, constants } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
 const copy = async () => {
-  // Write your code here
+  try {
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const src = path.join(__dirname, './files');
+    const dest = path.join(__dirname, './files_copy');
+    const files = await readdir(src);
+    await mkdir(dest);
+    for (const file of files) {
+      await copyFile(path.join(src, file), path.join(dest, file), constants.COPYFILE_EXCL);
+    }
+  } catch (err) {
+    console.error('FS operation failed');
+  }
 };
 
 copy();
