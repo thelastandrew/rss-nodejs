73b6044505951d42c652261674a7b9b309f3054b
thelastandrew <thelastandrew@gmail.com>
2022-11-30 01:49:58 +0300

    feat: fs-rename resolved

--- 01-nodejs-basics/src/fs/rename.js
+++ 01-nodejs-basics/src/fs/rename.js
@@ -1,5 +1,29 @@
-const rename = async () => {
-    // Write your code here 
+import { access, rename } from 'node:fs/promises';
+
+const fileExists = async (path) => {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
 };
 
-await rename();
+const renameFn = async () => {
+  try {
+    const wrong = new URL('./files/wrongFilename.txt', import.meta.url);
+    const proper = new URL('./files/properFilename.md', import.meta.url);
+    const exists = await fileExists(proper);
+    if (!exists) {
+        await rename(wrong, proper);
+        console.log('file renamed');
+    } else {
+        throw new Error;
+    }
+  } catch (err) {
+    console.error('FS operation failed');
+  }
+};
+
+await renameFn();
+
