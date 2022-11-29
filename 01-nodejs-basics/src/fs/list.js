072257c4197d9a213bc24ee79aa316ee90c0c5c8
thelastandrew <thelastandrew@gmail.com>
2022-11-29 23:51:12 +0300

    feat: fs-list resolved

--- 01-nodejs-basics/src/fs/list.js
+++ 01-nodejs-basics/src/fs/list.js
@@ -1,5 +1,13 @@
+import { readdir } from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const src = new URL('./files', import.meta.url);
+        const files = await readdir(src);
+        console.log(files);
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await list();
