7863d9a48e97215f7b7ad28779e2c834dcf5a110
thelastandrew <thelastandrew@gmail.com>
2022-11-29 20:55:52 +0300

    feat: fs-create resolved

--- 01-nodejs-basics/src/fs/create.js
+++ 01-nodejs-basics/src/fs/create.js
@@ -1,5 +1,13 @@
+import { writeFile } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here
+    try {
+        const filePath = new URL('./files/fresh.txt', import.meta.url);
+        const data = 'I am fresh and young';
+        await writeFile(filePath, data, { flag: 'wx' });
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await create();
