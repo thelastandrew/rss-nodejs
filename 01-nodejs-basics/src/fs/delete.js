9022a4046beb95975117fc889d9dc0ec729fad62
thelastandrew <thelastandrew@gmail.com>
2022-11-29 23:46:45 +0300

    feat: fs-delete resolved

--- 01-nodejs-basics/src/fs/delete.js
+++ 01-nodejs-basics/src/fs/delete.js
@@ -1,5 +1,13 @@
+import { rm } from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        const filePath = new URL('./files/fileToRemove.txt', import.meta.url);
+        await rm(filePath);
+        console.log('file deleted');
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await remove();
