be825ec406d3d99a497f7d587efef49e9cf06224
thelastandrew <thelastandrew@gmail.com>
2022-11-30 23:51:35 +0300

    feat: streams-read resolved

--- 01-nodejs-basics/streams/read.js
+++ 01-nodejs-basics/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const src = join(__dirname, './files/fileToRead.txt');
+    const readStream = createReadStream(src, 'utf-8');
+    readStream.pipe(process.stdout);
 };
 
 await read();
