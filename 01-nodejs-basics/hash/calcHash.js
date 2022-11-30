5d0fe1508592530b1497bcd0fcbdfe5cbbd4556d
thelastandrew <thelastandrew@gmail.com>
2022-11-30 23:39:20 +0300

    feat: hash resolved

--- 01-nodejs-basics/hash/calcHash.js
+++ 01-nodejs-basics/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const __dirname = fileURLToPath(new URL('.', import.meta.url));
+        const src = join(__dirname, './files/fileToCalculateHashFor.txt');
+        const fileBuffer = await readFile(src);
+        const hash = createHash('sha256');
+
+        hash.update(fileBuffer);
+
+        const hex = hash.digest('hex');
+
+        console.log(hex);
+    } catch (e) {
+        throw e;
+    }
 };
 
 await calculateHash();
