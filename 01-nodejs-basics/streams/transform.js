5c28383e3fc1d26f05e848a67e1c9e2224594445
thelastandrew <thelastandrew@gmail.com>
2022-12-01 00:41:12 +0300

    feat: streams-transform resolved

--- 01-nodejs-basics/streams/transform.js
+++ 01-nodejs-basics/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform, pipeline } from 'node:stream'
+
 const transform = async () => {
-    // Write your code here 
+    const readable = process.stdin;
+    const writable = process.stdout;
+    const transform = new Transform({
+        transform(chunk, enc, callback) {
+            const chunkStr = chunk.toString().trim();
+            const reverseChunk = chunkStr.split('').reverse('').join('');
+            this.push(`${reverseChunk}\n`);
+            callback();
+        }
+    });
+    pipeline(
+        readable,
+        transform,
+        writable,
+        err => { console.error(`Error: ${err}`) }
+    );
 };
 
 await transform();
