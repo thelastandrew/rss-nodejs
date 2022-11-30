856394894a3e67645b9f19b35c96dd230f3f7091
thelastandrew <thelastandrew@gmail.com>
2022-11-30 21:38:33 +0300

    feat: cli-argv resolved

--- 01-nodejs-basics/cli/args.js
+++ 01-nodejs-basics/cli/args.js
@@ -1,5 +1,15 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const argsArr = argv.slice(2);
+    const subRes = argsArr.map((arg, i) => {
+        return i%2 === 0 ? arg.slice(2) : arg;
+    });
+    const res = [];
+    for (let i = 0; i < subRes.length; i += 2) {
+        res.push(`${subRes[i]} is ${subRes[i + 1]}`);
+    }
+    console.log(res.join(', '));
 };
 
 parseArgs();
