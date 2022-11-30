2c913812fabc58a06efec61b56b679608183e13e
thelastandrew <thelastandrew@gmail.com>
2022-11-30 22:55:08 +0300

    feat: cli-env resolved

--- 01-nodejs-basics/cli/env.js
+++ 01-nodejs-basics/cli/env.js
@@ -1,5 +1,11 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const res = [];
+    for (const [key, value] of Object.entries(env)) {
+        if (key.substring(0, 4) === 'RSS_') res.push(`${key}=${value}`);
+    }
+    console.log(res.join('; '));
 };
 
 parseEnv();
