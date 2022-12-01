8c3d07e5955648dd37771119d686f39cc9fd5c92
thelastandrew <thelastandrew@gmail.com>
2022-12-01 20:50:19 +0300

    feat: zip-compress resolved

--- 01-nodejs-basics/zip/compress.js
+++ 01-nodejs-basics/zip/compress.js
@@ -1,5 +1,25 @@
+import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { rm } from 'node:fs/promises';
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const src = join(__dirname, './files/fileToCompress.txt');
+    const srcStream = createReadStream(src);
+    const destStream = createWriteStream(join(__dirname, './files/archive.gz'));
+    const gzip = createGzip();
+    const pipe = promisify(pipeline);
+
+    try {
+        await pipe(srcStream, gzip, destStream);
+        await rm(src);
+    } catch (e) {
+        throw e;
+    }
 };
 
 await compress();
