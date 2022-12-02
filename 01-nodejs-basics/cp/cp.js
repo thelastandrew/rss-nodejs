d99b7eb718dc55130e328ea51aff045b99ba09d9
thelastandrew <thelastandrew@gmail.com>
2022-12-03 00:16:56 +0300

    feaat: cp resolved

--- 01-nodejs-basics/cp/cp.js
+++ 01-nodejs-basics/cp/cp.js
@@ -1,5 +1,25 @@
+import { spawn }  from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const src = join(__dirname, './files/script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    let initiated = false;
+    const childProcess = spawn('node', [src, ...args.split(' ')]);
+
+    process.stdin.on('data', (msg) => {
+        childProcess.stdin.write(msg);
+    });
+
+    childProcess.stdout.on('data', (data) => {
+        console.log(data.toString());
+        if (!initiated) {
+            console.log('Type any data..\n');
+            initiated = true;
+        }
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess('--silent --all');
